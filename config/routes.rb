0a73541118b9818e0dd0d52beaae8a882ce809bb
Rhea Kaw <rck802@gmail.com>
2014-03-30 22:52:29 -0700

    added route for /available

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,8 @@ RheakawIs::Application.routes.draw do
 
   get "/resume" => "resume#index"
 
+  get "/available" => "resume#index"
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
